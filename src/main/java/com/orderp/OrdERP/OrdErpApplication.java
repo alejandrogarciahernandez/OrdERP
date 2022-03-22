dc3cff83b16d85a169afa84bcae1ce5f2964d925
Crw94 <alejandro.inves@gmail.com>
2022-03-22 23:55:09 +0100

    Initial commit

---
+++ src/main/java/com/orderp/OrdERP/OrdErpApplication.java
@@ -0,0 +1,13 @@
+package com.orderp.OrdERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderp/OrdERP/OrdErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderp.OrdERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
